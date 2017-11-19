7dae794c2e9f3db34bea15e5e687f35d6afe76be
kamil.berdychowski <k.berdychowski@gmail.com>
2017-11-19 13:27:34 +0100

    Starting multiplication table application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='main'>
+          <div className='title'>Tabliczka mnożenia</div>
+      </div>
+    );
+  }
+}
+
+export default App;
