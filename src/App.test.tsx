508aca7c26ea52cc0b27e2f43b9c3bf34147d03c
Kamil Berdychowski <k.berdychowski@gmail.com>
2024-11-23 22:30:56 +0100

    feat: allow to retake errors (#8)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+import {AnsweredQuestion, GameState, Question} from "./providers/MultiplicationTableStateProvider";
 
-function playGame(selectedNumber: string, container: HTMLElement) {
+function playGame(selectedNumber: string, container: HTMLElement, failOne = false) {
     let questionLabel = container.querySelector('.question');
+    let index = 1;
     do {
         const questionText = questionLabel?.textContent || '';
         const [firstNumber, secondNumber] = questionText.split(' * ');
         expect(firstNumber).toEqual(selectedNumber);
         expect(parseInt(secondNumber)).toBeGreaterThanOrEqual(1);
         expect(parseInt(secondNumber)).toBeLessThan(11);
-        const answer = parseInt(firstNumber) * parseInt(secondNumber);
+        let answer = parseInt(firstNumber) * parseInt(secondNumber);
+        if(failOne && index === 1) {
+            answer = answer - 1;
+        }
         let input = container.querySelector('#answer') as HTMLInputElement;
         fireEvent.change(input, {target: {value: answer.toString()}});
         fireEvent.submit(input!);
@@ -18,7 +23,14 @@ function playGame(selectedNumber: string, container: HTMLElement) {
         // after last answer the input is removed
         expect(input ? input.value : '').toBe('');
         questionLabel = container.querySelector('.question');
+        index++;
     } while (questionLabel);
+}
+
+function selectNumber(selectedNumber: string, container: HTMLElement) {
+    let number = screen.getByText(selectedNumber);
+    fireEvent.click(number);
+    fireEvent.click(container.querySelector('#startGame')!);
 }
 
 describe('Multiplcation Table Game', () => {
@@ -35,9 +47,22 @@ describe('Multiplcation Table Game', () => {
     test('plays game', () => {
         const {container} = render(<App/>);
         const selectedNumber = '2';
-        let number = screen.getByText(selectedNumber);
-        fireEvent.click(number);
-        fireEvent.click(container.querySelector('#startGame')!);
+        selectNumber(selectedNumber, container);
         playGame(selectedNumber, container);
+    });
+
+    test('after retaking wrong answers', () => {
+        const {container} = render(<App/>);
+        const selectedNumber = '2';
+        selectNumber(selectedNumber, container);
+
+        playGame(selectedNumber, container, true);
+
+        const retakeButton = container.querySelector('#retake') as HTMLButtonElement;
+        expect(retakeButton).toBeInTheDocument();
+        fireEvent.click(retakeButton!);
+        //
+        // let input = container.querySelector('input');
+        // expect(input).toBeInTheDocument();
     });
 });
